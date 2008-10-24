f183c53e9efb536274b1a0a37bcb5db5b2bceeab
netpete <netpete@d5f2ede4-3b4b-0410-b875-91a8e0809cc3>
2008-10-24 02:36:28 +0000

    updated setup.py to check for dependencies

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 import sys, os
 from setuptools import setup , find_packages
 
-mainscript = 'dmilo.py'
-if os.name == 'nt':
-	homedir = os.environ['HOMEPATH']
-else:
-	homedir = os.environ['HOME']
-
 packages = find_packages()
 setup(name = 'dMilo',
 	version='0.2',
 	description='3D Asset Management Application',
 	packages = packages,
-	#include_package_data = True,
-	entry_points = {'console_scripts': ['zipinstall = dmilo.zipinstall:main', 'dmilo = dmilo.dmilo:main' ]},
+	entry_points = {
+		'console_scripts': [
+			'zipinstall = dmilo.zipinstall:main',
+			'dmilo = dmilo.dmilo:main' 
+			]
+		},
 	package_data = {
 		'dmilo':['templates/*','*.xrc','resource/*'],
 		},
 	zip_safe=False,
-	
+	install_requires = [
+	#'wxPython',
+	#'twisted',
+	'mako',
+	'sqlobject',
+	'ZestyParser',	
+	],
 	author='Peter Tiegs',
 	author_email='peter@nascentia.com',
 	url='www.nascentia.com',
